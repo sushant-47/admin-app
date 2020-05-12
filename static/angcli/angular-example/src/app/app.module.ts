e5e038e54bf8c78402d8cb2260680445aabfff60
Sushant Kakroo <sushantkakroo47@gmail.com>
2020-05-12 13:34:15 +0530

    ang9 : extract css ; npm scripts added ; app2 component ;

--- static/angcli/angular-example/src/app/app.module.ts
+++ static/angcli/angular-example/src/app/app.module.ts
@@ -2,17 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { App2Component } from './app2.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		App2Component
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule
+	],
+	providers: [],
+	bootstrap: [App2Component]
 })
+
 export class AppModule { }
