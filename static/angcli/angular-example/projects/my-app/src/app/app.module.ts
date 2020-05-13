8a11d6b6d9443da212ee066322008739e3b00b26
Sushant Kakroo <sushantkakroo47@gmail.com>
2020-05-13 11:54:51 +0530

    ang9 : js from ang9 project ;

--- static/angcli/angular-example/projects/my-app/src/app/app.module.ts
+++ static/angcli/angular-example/projects/my-app/src/app/app.module.ts
@@ -2,17 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/root/app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
+
 export class AppModule { }
